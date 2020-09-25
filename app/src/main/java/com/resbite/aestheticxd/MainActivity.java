7e61d70f49763063fb2de20819b949626d19d497
resistencia1 <crisevil1@gmail.com>
2020-09-25 17:26:22 -0500

    sin navigation bottons

--- app/src/main/java/com/resbite/aestheticxd/MainActivity.java
+++ app/src/main/java/com/resbite/aestheticxd/MainActivity.java
@@ -21,11 +21,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+
+
+
         TextView textView = (TextView) findViewById(R.id.myTxtView);
         textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon, 0, 0, 0);
 
         VideoView videoView2=(VideoView)findViewById(R.id.videoView);
-        Uri path2 =Uri.parse("android.resource://com.resbite.aestheticxd/"+R.raw.aestdh2);
+        Uri path2 =Uri.parse("android.resource://com.resbite.aestheticxd/"+R.raw.nujare);
         videoView2.setVideoURI(path2);
         videoView2.setMediaController(new MediaController(this));
         videoView2.start();
@@ -45,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 if(isSwitchOn){
                     lottieSwitchButton.setMinAndMaxProgress(0.0f,0.5f);
                     lottieSwitchButton.playAnimation();
-                    ralativeLayout.setBackgroundResource(R.drawable.sak2);
+                    ralativeLayout.setBackgroundResource(R.drawable.windowsxp);
 
 
 
@@ -54,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     lottieSwitchButton.setMinAndMaxProgress(0.5f,1.0f);
                     lottieSwitchButton.playAnimation();
-                    ralativeLayout.setBackgroundResource(R.drawable.sak3);
+                    ralativeLayout.setBackgroundResource(R.drawable.windowsnoch);
 
 
 
